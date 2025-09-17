48cfb8fb357f3fa8ab5c3c7e0e4bc556405ac40a
Ibrahima ndiaye <49150724+ibrahima411@users.noreply.github.com>
2025-09-17 17:21:37 +0000

    activité

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -38,34 +38,85 @@ export default function Dashboard() {
 				<div className="row">
 					<div className="col-xl-12 col-xxl-12">
 						<div className="row">
-							<div className="col-sm-4">
-								<div className="card avtivity-card">
-									<div className="card-body">
-										<div className="media align-items-center">
-											<span className="activity-icon bgl-success me-md-4 me-3">
-												<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-													<g clip-path="url(#clip2)">
-													<path d="M14.6406 24.384C14.4639 24.1871 14.421 23.904 14.5305 23.6633C15.9635 20.513 14.4092 18.7501 14.564 11.6323C14.5713 11.2944 14.8346 10.9721 15.2564 10.9801C15.6201 10.987 15.905 11.2962 15.8971 11.6598C15.8902 11.9762 15.8871 12.2939 15.8875 12.6123C15.888 12.9813 16.1893 13.2826 16.5583 13.2776C17.6426 13.2628 19.752 12.9057 20.5684 10.4567L20.9744 9.23876C21.7257 6.9847 20.4421 4.55115 18.1335 3.91572L13.9816 2.77294C12.3274 2.31768 10.5363 2.94145 9.52387 4.32498C4.66826 10.9599 1.44452 18.5903 0.0754914 26.6727C-0.300767 28.8937 0.754757 31.1346 2.70222 32.2488C13.6368 38.5051 26.6023 39.1113 38.35 33.6379C39.3524 33.1709 40.0002 32.1534 40.0002 31.0457V19.1321C40.0002 18.182 39.5322 17.2976 38.7484 16.7664C34.5339 13.91 29.1672 14.2521 25.5723 18.0448C25.2519 18.3828 25.3733 18.937 25.8031 19.1166C27.4271 19.7957 28.9625 20.7823 30.2439 21.9475C30.5225 22.2008 30.542 22.6396 30.2654 22.9155C30.0143 23.1658 29.6117 23.1752 29.3485 22.9376C25.9907 19.9053 21.4511 18.5257 16.935 19.9686C16.658 20.0571 16.4725 20.3193 16.477 20.61C16.496 21.8194 16.294 22.9905 15.7421 24.2172C15.5453 24.6544 14.9607 24.7409 14.6406 24.384Z" fill="#27BC48"/>
-													</g>
-													<defs>
-													<clipPath id="clip2">
-													<rect width="40" height="40" fill="white"/>
-													</clipPath>
-													</defs>
+							<div className="col-xl-3 col-lg-6 col-sm-6">
+							<div className="widget-stat card">
+								<div className="card-body p-4">
+									<div className="media ai-icon">
+										<span className="me-3 bgl-primary text-primary">
+											<svg id="icon-customers" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-user">
+												<path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+												<circle cx="12" cy="7" r="4"></circle>
+											</svg>
+										</span>
+										<div className="media-body">
+											<p className="mb-1">Nombre d&apos;Atéliers</p>
+											<h4 className="mb-0">{dashboard?.shops.total}</h4>
+											<span className="badge badge-primary">+3.5%</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							</div>
+							<div className="col-xl-3 col-lg-6 col-sm-6">
+								<div className="widget-stat card">
+									<div className="card-body p-4">
+										<div className="media ai-icon">
+											<span className="me-3 bgl-warning text-warning">
+												<svg id="icon-orders" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-file-text">
+													<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+													<polyline points="14 2 14 8 20 8"></polyline>
+													<line x1="16" y1="13" x2="8" y2="13"></line>
+													<line x1="16" y1="17" x2="8" y2="17"></line>
+													<polyline points="10 9 9 9 8 9"></polyline>
 												</svg>
 											</span>
 											<div className="media-body">
-												<p className="fs-14 mb-2">Couturiers</p>
-												<span className="title text-black font-w600">{dashboard?.users.total} Couturiers</span>
-											</div>
-										</div>
-										<div className="progress" style={{height:"5px"}}>
-											<div className="progress-bar bg-success" style={{width: "42%", height:"5px"}} aria-label="Progess-success" role="progressbar">
-												<span className="sr-only">42% Complete</span>
+												<p className="mb-1">Total commande</p>
+												<h4 className="mb-0">{dashboard?.orders.total}</h4>
+												<span className="badge badge-warning">+3.5%</span>
 											</div>
 										</div>
 									</div>
-									<div className="effect bg-success"></div>
+								</div>
+							</div>
+
+							<div className="col-xl-3 col-lg-6 col-sm-6">
+								<div className="widget-stat card">
+									<div className="card-body p-4">
+										<div className="media ai-icon">
+											<span className="me-3 bgl-success text-success">
+												<svg id="icon-database-widget" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-database">
+													<ellipse cx="12" cy="5" rx="9" ry="3"></ellipse>
+													<path d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3"></path>
+													<path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"></path>
+												</svg>
+											</span>
+											<div className="media-body">
+												<p className="mb-1">Nombre de transactions</p>
+												<h4 className="mb-0">{dashboard?.transactions.total}	</h4>
+												<span className="badge badge-success">-3.5%</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-xl-3 col-lg-6 col-sm-6">
+								<div className="widget-stat card">
+									<div className="card-body  p-4">
+										<div className="media ai-icon">
+											<span className="me-3 bgl-danger text-danger">
+												<svg id="icon-revenue" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-dollar-sign">
+													<line x1="12" y1="1" x2="12" y2="23"></line>
+													<path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
+												</svg>
+											</span>
+											<div className="media-body">
+												<p className="mb-1">Abonnement {dashboard?.subscriptions.byType[0].name}</p>
+												<h4 className="mb-0">364.50K</h4>
+												<span className="badge badge-danger">-3.5%</span>
+											</div>
+										</div>
+									</div>
 								</div>
 							</div>
 							<div className="col-sm-4">
